1a7cdf31260688a83eabc44fcc61d7f7f25c29ec
Evgnius <shtyrev.evgenii@gmail.com>
2021-11-22 16:26:11 +0700

    First js console aplication

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log('arr' + "- obj");
+const numberOfFlilms = +prompt('Сколько фильмов вы уже посмотрели','');
+
+const personalMovieDB = {
+    count: numberOfFlilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из послених просмотреных фильмов', '');
+const b = prompt('На сколько оценили его', '');
+const c = prompt('Один из послених просмотреных фильмов', '');
+const d = prompt('На сколько оценили его', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
