86c1e64aa93732111e2f46979692a94fc23e4234
Evgnius <shtyrev.evgenii@gmail.com>
2021-11-22 18:24:23 +0700

    Stage 2 For and if

--- js/myscript.js
+++ js/myscript.js
@@ -10,12 +10,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из послених просмотреных фильмов', '');
-const b = prompt('На сколько оценили его', '');
-const c = prompt('Один из послених просмотреных фильмов', '');
-const d = prompt('На сколько оценили его', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i=0; i < 2; i++) {
+    const a = prompt('Один из послених просмотреных фильмов', '');
+    const b = prompt('На сколько оценили его', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов'); 
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
