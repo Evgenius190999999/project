890dca0851248872c462b267e7aeb702344cbc32
Evgnius <shtyrev.evgenii@gmail.com>
2021-12-13 13:07:33 +0700

    Finaly release

--- Food_dist/js.js
+++ Food_dist/js.js
@@ -69,11 +69,115 @@
 
 // console.log(box.getBoundingClientRect());
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-}
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`hello ${this.name}`);
+//     };
+// }
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} ушел`);
+// };
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+// ivan.exit();
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
+
+// class Rectangle {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//     constructor(height, width, text, bgColor) {
+//         super(height, width);
+//         this.text = text;
+//         this.bgColor = bgColor;
+//     }
+
+//     showMyProps() {
+//         console.log(`Текст: ${this.text}, цвет ${this.bgColor}`);
+//     }
+// }
+
+// const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
+
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
+
+// const log = function(a, b, ...rest) {
+//     console.log(a, b, rest);
+// };
+
+// log('basic','rest', 'operatot', 'usage');
+
+// function calcOrDouble(number, basis = 2) {
+//     console.log(number * basis);
+// }
+
+// calcOrDouble(3);
 
 
-const ivan = new User('Ivan', 28);
+// const persone = {
+//     name: 'alex',
+//     tel: '+9999999',
+//     parents: {
+//         mom: 'olga',
+//         dad: 'mike'
+//     }
+// };
+
+
+// const clone = JSON.parse(JSON.stringify(persone));
+
+// clone.parents.mom = 'Ann';
+
+// console.log(persone);
+// console.log(clone);
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const date = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / date.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    });
+
+    // status
+    // statusText
+    // Response
+    // readyState
+
+});
--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -94,8 +94,8 @@ window.addEventListener('DOMContentLoaded', () => {
     // Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
+
     
     modalTrigger.forEach(btn => {
             btn.addEventListener('click', openModal);
@@ -114,11 +114,9 @@ function openModal() {
         document.body.style.overflow = '';
 }
     
-    modalCloseBtn.addEventListener('click',closeModal);
-
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            closeModal();
+        if (e.target === modal || e.target.getAttribute('data-close') == "") {
+        closeModal();
         }
     });
 
@@ -214,4 +212,78 @@ function openModal() {
         'menu__item',
     ).render();
 
+    // Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item =>{
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const requrst = new XMLHttpRequest();
+            requrst.open('POST', 'server.php');
+
+            requrst.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formDate = new FormData(form);
+
+            const object = {};
+            formDate.forEach(function(value, key){
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            requrst.send(json);
+
+            requrst.addEventListener('load', () => {
+                if (requrst.status === 200) {
+                    console.log(requrst.response);
+                    showThanksModal(message.success);
+                    form.reset();
+                    statusMessage.remove();
+                } else {
+                    showThanksModal(message.failure);
+                }
+            });
+        });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title>${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
 });
